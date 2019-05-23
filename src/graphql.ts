f4e7568f4bf8dfed01dce87611e49f966798eb49
jiangzhuo <glmmmmmm@163.com>
2019-05-23 14:47:24 +0800

    init

--- src/graphql.ts
+++ src/graphql.ts
@@ -5,7 +5,12 @@ import { eventContext } from 'aws-serverless-express/middleware';
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { async } from "rxjs/internal/scheduler/async";
 
+// NOTE: If you get ERR_CONTENT_DECODING_FAILED in your browser, this is likely
+// due to a compressed response (e.g. gzip) which has not been handled correctly
+// by aws-serverless-express and/or API Gateway. Add the necessary MIME types to
+// binaryMimeTypes below
 const binaryMimeTypes: string[] = [];
 
 let cachedServer: Server;
@@ -19,26 +24,38 @@ process.on('uncaughtException', reason => {
 });
 
 async function bootstrapServer(): Promise<Server> {
-
   if (!cachedServer) {
     try {
-      const expressApp = require('express')();
-      const nestApp = await NestFactory.create(AppModule, expressApp);
+      // const expressApp = require('express')();
+      const nestApp = await NestFactory.create(AppModule);
       nestApp.use(eventContext());
       await nestApp.init();
-      cachedServer = createServer(expressApp, undefined, binaryMimeTypes);
-    }
-    catch (error) {
+      cachedServer = createServer(nestApp.getHttpAdapter().getInstance(), undefined, binaryMimeTypes);
+    } catch (error) {
       return Promise.reject(error);
     }
   }
   return Promise.resolve(cachedServer);
 }
 
+export const initializer = async (context, callback) => {
+  cachedServer = await bootstrapServer();
+  callback()
+};
+
 export const handler: Handler = async (
   event: any,
-  context: Context
+  context: Context,
+  callback,
 ) => {
-  cachedServer = await bootstrapServer();
-  return proxy(cachedServer, event, context, 'PROMISE').promise;
+  console.log(event.toString());
+  proxy(cachedServer, JSON.parse(event.toString()), context, 'CALLBACK', callback);
+  // callback(null, {
+  //   statusCode: 200,
+  //   body: {
+  //     data: {
+  //       hello: 'Hello world1 nest!',
+  //     },
+  //   },
+  // });
 };
